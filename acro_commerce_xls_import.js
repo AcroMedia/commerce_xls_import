db7f429d9b8bd8fb27341952be4070ab70696882
cbanman <cbanman@acromediainc.com>
2015-06-19 11:44:29 -0700

    Automatic fixed made by Code Sniffer

--- acro_commerce_xls_import.js
+++ acro_commerce_xls_import.js
@@ -1,46 +1,45 @@
 /**
- *	@file acro_commerce_xls_import.js
+ * @file acro_commerce_xls_import.js
 
- *  Custom JavaScript for the Acromedia Commerce xls import
+ *  Custom JavaScript for the Acromedia Commerce xls import.
  */
 
 var timer;
 (function ($) {
-	$(document).ready(function(){
-		$('#edit-import-button').click(function(){
-			$('#import_status').val(1);
-		});
+  $(document).ready(function(){
+    $('#edit-import-button').click(function(){
+      $('#import_status').val(1);
+    });
 
-		$('#edit-import-stop').click(function() {
-			$('#import_status').val(0);
-			clearTimeout(timer);
-			//some code to update the message
-		});
+        $('#edit-import-stop').click(function() {
+          $('#import_status').val(0);
+          clearTimeout(timer);
+          // Some code to update the message.
+        });
 
-		if($('input[name="import_status"]').val() == 1){
-			//start counting after 2 seconds;
-			timer = setInterval(function(){start_updates()}, 2000);
-		}
-	});
-	function start_updates() {
-		periodic_update_import_status();
-	}
-	function periodic_update_import_status() {
-		$.ajax({
-			url: Drupal.settings.basePath + "?q=admin/commerce/products/import_commerce/get-import-status",
-			type: "POST",
-			dataType: "json",
-			data: {},
-			success: function(data){
-				var message_box = $('#import_status_messages');
-				message_box.css('display', 'block');
-				message_box.html(data.message);
-				if(data.status == 1){
-					location.reload();
-				}
-			}
+        if ($('input[name="import_status"]').val() == 1) {
+          // Start counting after 2 seconds;
+          timer = setInterval(function(){start_updates()}, 2000);
+        }
+  });
+  function start_updates() {
+    periodic_update_import_status();
+  }
+  function periodic_update_import_status() {
+    $.ajax({
+      url: Drupal.settings.basePath + "?q=admin/commerce/products/import_commerce/get-import-status",
+      type: "POST",
+      dataType: "json",
+      data: {},
+      success: function(data){
+        var message_box = $('#import_status_messages');
+        message_box.css('display', 'block');
+        message_box.html(data.message);
+        if (data.status == 1) {
+          location.reload();
+        }
+      }
 
-		});
-	}
+    });
+  }
 })(jQuery);
-
