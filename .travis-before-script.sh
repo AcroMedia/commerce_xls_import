3c6831544f85315ef84cfbb85228d0bc3d6ab94a
Shawn McCabe <smccabe@acromediainc.com>
2017-05-26 13:28:17 -0700

    Update tests for travis-ci

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -15,8 +15,8 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 # For D8, this is modules
 mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 
-drush dl commerce
-drush dl addressfield rules ctools entity views
+drush dl -y commerce
+drush dl -y addressfield rules ctools entity views
 drush en -y commerce_cart commerce_customer_ui commerce_product_ui commerce_line_item_ui commerce_order_ui commerce_payment commerce_payment_example commerce_tax_ui simpletest
 drush en -y commerce_xls_import
 git clone https://github.com/box/spout.git
